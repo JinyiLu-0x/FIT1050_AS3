2427f15f738975c78486e788fd90a2d5e09dcb6f
JinyiLu-0x <lujinyi2006@gmail.com>
2026-05-18 12:49:17 +1000

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,4 +81,74 @@
 
 		}
 
+	// Photo gallery section
+	// The auto-scrolling effect is adapted from reference code.
+	// https://www.w3schools.com/howto/howto_css_image_gallery_scroll.asp
+	// https://developer.mozilla.org/en-US/docs/Web/API/Window/setInterval
+	// https://stackoverflow.com/questions/25685486/how-to-show-webkit-scrollbar-which-has-been-hidden-using-displaynone-css-proper
+
+	(function() {
+
+			var collectionWindow = document.getElementById('collection-window');
+			var collectionTrack = document.querySelector('.collection-track');
+			var collectionItems = document.querySelectorAll('.collection-item');
+			var collectionModal = document.getElementById('collection-modal');
+			var collectionModalImage = document.getElementById('collection-modal-image');
+			var collectionModalClose = document.getElementById('collection-modal-close');
+			var pauseCollection = false;
+
+			if (collectionWindow && collectionTrack) {
+				collectionWindow.addEventListener('mouseenter', function() {
+					pauseCollection = true;
+				});
+
+				collectionWindow.addEventListener('mouseleave', function() {
+					pauseCollection = false;
+				});
+
+				/* Auto-scroll function */
+				window.setInterval(function() {
+					if (pauseCollection)
+						return;
+
+					collectionWindow.scrollLeft += 1;
+
+					if (collectionWindow.scrollLeft >= collectionWindow.scrollWidth - collectionWindow.clientWidth)
+						collectionWindow.scrollLeft = 0;
+				}, 30);
+			}
+
+			if (collectionItems.length === 0 || !collectionModal || !collectionModalImage || !collectionModalClose)
+				return;
+
+			// Click event for each collection item to open the modal
+			collectionItems.forEach(function(item) {
+				item.addEventListener('click', function() {
+					var clickedImage = item.querySelector('img');
+
+					if (!clickedImage)
+						return;
+
+					collectionModalImage.src = clickedImage.src;
+					collectionModalImage.alt = clickedImage.alt;
+					collectionModal.classList.add('is-visible');
+					collectionModal.setAttribute('aria-hidden', 'false');
+				});
+			});
+
+			collectionModalClose.addEventListener('click', function() {
+				collectionModal.classList.remove('is-visible');
+				collectionModal.setAttribute('aria-hidden', 'true');
+			});
+
+			collectionModal.addEventListener('click', function(event) {
+				if (!event.target.closest('.collection-modal-inner')) {
+					collectionModal.classList.remove('is-visible');
+					collectionModal.setAttribute('aria-hidden', 'true');
+				}
+			});
+
+		})();
+
+
 })(jQuery);
